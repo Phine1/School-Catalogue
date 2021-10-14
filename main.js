1e5a5a378dd2cdebbcc5be8548f8d534970354f8
Phine1 <phinehas.sersah@amalitech.org>
2021-10-14 12:58:41 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+
+// The parent class
+
+class School {
+    constructor(name,level,numberOfStudents) {
+      this._name=name;
+      this._level = level;
+      this._numberOfStudents=numberOfStudents;
+    }
+    get name() {
+      return this._name;
+    }
+    get level() {
+      return this._level;
+    }
+    get numberOfStudents() {
+      return this._numberOfStudents
+    }
+    set numberOfStudents(newNumberOfStudent) {
+      if(newNumberOfStudent===!NaN) {
+        this._numberOfStudents=newNumberOfStudent
+      } else {
+        console.log('Invalid input: numberOfStudents must be set to a Number.')
+      }
+    }
+    quickFacts() {
+      console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level`)
+    }
+    static pickSubstituteTeacher(substituteTeachers) {
+   let array = Math.floor(Math.random()*substituteTeachers.length)
+   return substituteTeachers[array]
+    }
+   }
+
+   // Child Class extending parent class
+
+   class PrimarySchool extends School {
+     constructor(name,numberOfStudents,pickupPolicy){
+       super(name,'primary',numberOfStudents);
+       this._pickupPolicy = pickupPolicy;
+     }
+     get pickupPolicy() {
+       return this._pickupPolicy;
+     }
+   }
+
+   // Child Class extending parent class
+
+   class HighSchool extends School {
+     constructor(name,numberOfStudents,sportsTeams) {
+       super(name,'high',numberOfStudents);
+       this._sportsTeams = sportsTeams;
+     }
+     get sportsTeams() {
+       return console.log(this._sportsTeams)
+     }
+   }
+   
+
+   // Implementing  the classes created
+   
+   const lorraineHansbury = new PrimarySchool('Lorraine Hansbury',514,"Students must be picked up by a parent, guardian, or a family member over the age of 13.")
+   lorraineHansbury.quickFacts();
+   const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli'])
+   const alSmith = new HighSchool('Al E. Smith',415,['Baseball', 'Basketball', 'Volleyball', 'Track and Field'])
+   console.log(alSmith.sportsTeams)
+   
+   
+   
